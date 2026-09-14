d0a8fd38fddc32e69d63a54ff083b6de71f074fb
Axel DUPUY <axel_dupuy42000@hotmail.fr>
2026-09-14 21:16:16 +0200

    security: configure HTTPS production settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -5,4 +5,30 @@ DEBUG = False
 ALLOWED_HOSTS = [
     "mecamountain.fr",
     "www.mecamountain.fr",
+    "127.0.0.1",
+    "localhost",
 ]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST", "127.0.0.1"),
+        "PORT": os.getenv("DB_PORT", "5432"),
+    }
+}
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://mecamountain.fr",
+    "https://www.mecamountain.fr",
+]
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
